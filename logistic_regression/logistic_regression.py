24d104fa2a3848aa87052322b0a470686ddcb0e5
MikaelShahly <shahlyma@gmail.com>
2023-08-27 12:31:46 +0200

    make a test commit

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
+# Making an extra comment
 
 
 class LogisticRegression:
